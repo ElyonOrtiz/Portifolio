2bf86bd6e772efc2cd7a031d9bef21d429a9a1bd
Elyon <elyon.ortiz08@gmail.com>
2023-06-08 09:12:55 -0400

    page tsx alter workExperience

--- app/page.tsx
+++ app/page.tsx
@@ -64,8 +64,7 @@ const getPageData = async (): Promise<HomePageData> => {
 }
 export default async function Home() {
   const {page: pageData, workExperiences} = await getPageData();
-  console.log(pageData);
-
+  console.log(pageData, workExperiences);
     return (
     <>
       <HeroSection homeInfo={pageData}/>
