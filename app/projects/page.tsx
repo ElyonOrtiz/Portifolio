5a31b16a6c5660abad0a0bdfe8a1d258af9936a0
Elyon <elyon.ortiz08@gmail.com>
2023-08-04 19:52:43 -0400

    debug page/home

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -32,8 +32,7 @@ const getPageData = async (): Promise<ProjectsPageData> => {
 
 export default async function Projects() {
    const { projects } = await getPageData()
-   console.log(projects)
-   return (
+   return projects && (
       <>
         <PageIntroduction />
         <ProjectsList projects={projects}/>
