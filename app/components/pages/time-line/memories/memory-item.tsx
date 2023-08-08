ebde33957ba8d17f921b9b839f17dddfcc494b92
Elyon <elyon.ortiz08@gmail.com>
2023-08-08 17:47:36 -0400

    correction Date in section about me

--- app/components/pages/time-line/memories/memory-item.tsx
+++ app/components/pages/time-line/memories/memory-item.tsx
@@ -1,10 +1,8 @@
-
-import { SectionTitle } from '@/app/components/section-title'
-import { ExperienceItem } from '../../home/work-experience/experience-items'
 import Image from 'next/image'
-import { RxComponentBoolean } from "react-icons/rx"
 import { RichText } from '@/app/components/rich-text'
 import { Memory } from '@/app/types/aboutMe'
+import { format } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 
 
 type IMemoryProps = {
@@ -12,10 +10,16 @@ type IMemoryProps = {
 }
 
 export const MemoryItem= ({memory}: IMemoryProps ) => {
+  console.log(memory.date)
+  const date = new Date(memory.date);
+  const formattedDate = format(date, "dd/MM/yyyy", { locale: ptBR }).toString();
+
   return (
     <section className="container pb-3 flex gap-2 md:gap-4 lg:gap-6 flex-col md:flex-row">
         <div className="flex flex-col flex-1">
-          <span className='-ml-8 flex items-center gap-2 pb-8 w-full text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50'>/01/2008/</span>
+          <span className='-ml-8 flex items-center gap-2 pb-8 w-full text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50'>
+            {formattedDate}
+          </span>
            <div className="sm:w-full rounded-s-sm">
             <Image
               src={memory.imageMemory.url}
